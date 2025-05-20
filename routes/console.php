a5843e443a2509b98acbec7984c2a3c27fcec041
Harshvardhan Sharma <hvsharma.railway@gmail.com>
2025-05-20 15:03:10 +0530

    debug bar and db update added

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::job(new DeficiencyReminderScheduledJob)->dailyAt('10:00');
 Schedule::job(new DeficiencyReminderScheduledJob)->dailyAt('16:00');
 Schedule::job(new DeleteTemporaryUploadJob)->dailyAt('00:10');
+Schedule::command('db:dump-and-email')->dailyAt('01:00');
